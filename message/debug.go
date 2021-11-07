dc1c596f9235bb9154e468ace779fc8af949329c
DazFather <the6daz6@gmail.com>
2021-11-07 20:55:31 +0100

    Create a new debug function

---
+++ message/debug.go
@@ -0,0 +1,39 @@
+package message
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/NicoNex/echotron/v3"
+)
+
+/* Useful command to show what values the data is carrying by using JSON.
+ * tips: Be careful to who you are sending the message or the end user could
+ *       be a bit confused. If you are the developer use your chatID
+ */
+func Log(api echotron.API, chatID int64, any ...interface{}) {
+	var message = Text{
+		fmt.Sprint("🦜 <b>Log</b> [", time.Now(), "]\n"),
+		&echotron.MessageOptions{ParseMode: echotron.HTML},
+	}
+
+	// Parsing each data and add the result to the message text
+	for i, value := range any {
+		if data, e := json.MarshalIndent(value, "", "   "); e == nil {
+			message.Text = string(data)
+		} else {
+			message.Text = "Impossible to parse data"
+		}
+		message.Text = fmt.Sprint("\nData (", i, "):\n<code>", message.Text, "</code>")
+	}
+
+	// Clipping useful links
+	message.ClipInlineKeyboard([][]echotron.InlineKeyboardButton{{
+		{Text: "📖 Echotron", URL: "https://pkg.go.dev/github.com/NicoNex/echotron/v3"},
+		{Text: "📖 Telegram", URL: "https://core.telegram.org/bots/api"},
+	}})
+
+	// Send the message to the specified user
+	message.Send(api, chatID)
+}
