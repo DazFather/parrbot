b4343f64cf6b32f78648212cc8077b790d5e9fad
DazFather <the6daz6@gmail.com>
2021-11-07 21:05:51 +0100

    Add Dynamic menus and new package for stuffs like this

---
+++ tgui/menu.go
@@ -0,0 +1,143 @@
+package tgui // TeleGram User Interface or Toolkit for Graphical User Interface
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"Parrbot/message"
+	"Parrbot/robot"
+
+	"github.com/NicoNex/echotron/v3"
+)
+
+/* A menu is a collection of message that can be seen by pressing the related
+ * button. The menu can be triggered by both a message and a CallbackData.
+ */
+type Menu struct {
+	// The collection of pages of the menu, required to work
+	Pages []MenuPage
+
+	/* The caption of the navigation buttons. You can embed "[INDEX]" inside
+	 * PreviousCaption and NextCaption to show the number of the related page.
+	 * By default (if missing or empty string are passed) their values is:
+	 * "Next ⏭ [INDEX]", "[INDEX] ⏮ Prev.", "❌ Close".
+	 */
+	NextCaption, PreviousCaption, CloseCaption string
+}
+
+// A page of the menu is a function that will return the content that will be shown
+type MenuPage func(b *robot.Bot) (content string, opts *echotron.MessageTextOptions)
+
+// Implements a menu to work with a specific trigger (you can direclt add to the command list)
+func (m *Menu) UseMenu(trigger string) robot.Command {
+	// Set default captions values
+	if m.NextCaption == "" {
+		m.NextCaption = "Next ⏭ [INDEX]"
+	}
+	if m.PreviousCaption == "" {
+		m.PreviousCaption = "[INDEX] ⏮ Prev."
+	}
+	if m.CloseCaption == "" {
+		m.CloseCaption = "❌ Close"
+	}
+
+	// Create the handler function
+	var menuHandler robot.CommandFunc = func(bot *robot.Bot, update *message.Update) message.Any {
+		// Get the payload
+		var text string
+		if update.Message != nil {
+			text = update.Message.Text
+			bot.DeleteMessage(update.Message.From.ID, update.Message.ID)
+		} else {
+			text = update.CallbackQuery.Data
+		}
+		text = strings.TrimSpace(strings.TrimPrefix(text, trigger))
+
+		// Select the page of the menu
+		switch text {
+		case "":
+			return m.SelectPage(trigger, 0, bot, update)
+
+		case "x":
+			if update.CallbackQuery != nil {
+				bot.DeleteMessage(update.CallbackQuery.From.ID, update.CallbackQuery.Message.ID)
+			}
+
+		default:
+			n, err := strconv.Atoi(text)
+			if n >= 0 && n < len(m.Pages) && err == nil {
+				return m.SelectPage(trigger, n, bot, update)
+			}
+		}
+
+		return nil
+	}
+
+	// Create the command and return it
+	return robot.Command{
+		Trigger: trigger,
+		ReplyAt: message.CALLBACK_QUERY + message.MESSAGE,
+		Scope:   menuHandler,
+	}
+}
+
+// Select and use a page of the menu
+func (m Menu) SelectPage(trigger string, pageNumber int, b *robot.Bot, u *message.Update) message.Any {
+	var content, opt = m.Pages[pageNumber](b)
+	if opt == nil {
+		opt = new(echotron.MessageTextOptions)
+	}
+	opt.ReplyMarkup.InlineKeyboard = append(opt.ReplyMarkup.InlineKeyboard, m.genButtons(trigger, pageNumber))
+
+	if u.Message != nil {
+		msg := message.Text{
+			Text: content,
+			Opts: &echotron.MessageOptions{
+				ParseMode:             opt.ParseMode,
+				Entities:              opt.Entities,
+				DisableWebPagePreview: opt.DisableWebPagePreview,
+				ReplyMarkup:           opt.ReplyMarkup,
+			},
+		}
+		return msg
+	}
+
+	msgID := echotron.NewMessageID(u.CallbackQuery.From.ID, u.CallbackQuery.Message.ID)
+	b.EditMessageText(content, msgID, opt)
+	return nil
+
+}
+
+// Return the previous / close / next buttons row
+func (m Menu) genButtons(trigger string, pageNumber int) (btnRow []echotron.InlineKeyboardButton) {
+	// Previous Button
+	if pageNumber > 0 {
+		btnRow = append(btnRow, echotron.InlineKeyboardButton{
+			Text:         strings.ReplaceAll(m.PreviousCaption, "[INDEX]", strconv.Itoa(pageNumber)),
+			CallbackData: fmt.Sprint(trigger, " ", pageNumber-1),
+		})
+	}
+
+	// Close Button
+	btnRow = append(btnRow, echotron.InlineKeyboardButton{
+		Text:         m.CloseCaption,
+		CallbackData: fmt.Sprint(trigger, " x"),
+	})
+
+	// Next Button
+	if pageNumber < len(m.Pages)-1 {
+		btnRow = append(btnRow, echotron.InlineKeyboardButton{
+			Text:         strings.ReplaceAll(m.NextCaption, "[INDEX]", strconv.Itoa(pageNumber+2)),
+			CallbackData: fmt.Sprint(trigger, " ", pageNumber+1),
+		})
+	}
+	return
+}
+
+// Create a page of a menu that will return always the same output
+func StaticPage(content string, opt *echotron.MessageTextOptions) MenuPage {
+	return func(*robot.Bot) (string, *echotron.MessageTextOptions) {
+		return content, opt
+	}
+}
